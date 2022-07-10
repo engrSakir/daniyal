60ae8f94bcc92c2920a43aabbafcf6ec02c8fad8
Md. Sakir Ahmed <52326976+sakir-developer@users.noreply.github.com>
2022-07-10 14:15:23 +0600

    Update category.blade.php

--- resources/views/livewire/manager/category.blade.php
+++ resources/views/livewire/manager/category.blade.php
@@ -76,7 +76,7 @@
         <div class="modal-dialog modal-lg modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header bg-primary">
-                    <h5 class="modal-title text-white"><i class="fa fa-star"></i> Form</h5>
+                    <h5 class="modal-title text-white"><i class="fa fa-star"></i>Category Form</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
