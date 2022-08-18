2271d3207bad14075d5d77e5ac6f2e6b1adc414b
unknown <sakir.py>
2022-08-18 11:03:28 +0600

    expense delete

--- app/Http/Livewire/Manager/Expense.php
+++ app/Http/Livewire/Manager/Expense.php
@@ -13,12 +13,17 @@ class Expense extends Component
 {
     use LivewireAlert, WithFileUploads;
 
-    public $expense_fields = [];
+    public $expense_fields = [], $expense_date;
+
+    public function mount(){
+        $this->expense_date = date('Y-m-d');
+    }
+
     public function render()
     {
         return view('livewire.manager.expense',[
             'expense_categories' => ExpenseCategory::all(),
-            'expenses' => ModelsExpense::whereDate('created_at', Carbon::today())->get()
+            'expenses' => ModelsExpense::whereDate('created_at', $this->expense_date)->get()
         ]);
     }
 
@@ -26,7 +31,7 @@ class Expense extends Component
         $this->validate([
             "expense_fields.$expense_category_id.amount" => 'required|numeric|min:0'
         ]);
-        ModelsExpense::create([
+        ModelsExpense::insert([
             'category_id' => $expense_category_id,
             'creator_id' => auth()->user()->id,
             'amount' => $this->expense_fields[$expense_category_id]['amount'],
@@ -34,9 +39,21 @@ class Expense extends Component
             'description' => null,
             'image' => null,
             'date' => Carbon::now(),
+            'created_at' => Carbon::parse($this->expense_date. date(' h:i:s')),
         ]);
         $this->expense_fields[$expense_category_id]['amount'] = $this->expense_fields[$expense_category_id]['title'] = null;
         $this->alert('success', 'Success');
+    }
 
+    public function select_for_delete($id)
+    {
+        $this->select_for_delete = ModelsExpense::find($id);
+    }
+
+    public function delete()
+    {
+        $this->select_for_delete->delete();
+        $this->select_for_delete = null;
+        $this->alert('success', 'Success');
     }
 }
--- resources/views/livewire/manager/expense.blade.php
+++ resources/views/livewire/manager/expense.blade.php
@@ -9,31 +9,32 @@
                 <li class="breadcrumb-item active" aria-current="page">Expense Page</li>
             </ol>
         </div>
-        <div class="col-sm-3">
-            <div class="btn-group float-sm-right">
-                <button type="button" class="btn btn-outline-primary waves-effect waves-light" data-toggle="modal" data-target="#modal"><i class="fa fa-plus mr-1"></i> Create </button>
-            </div>
-        </div>
     </div>
     <!-- End Breadcrumb-->
     <div class="row">
         <div class="col-12 col-sm-6 col-lg-6 col-xl-3">
             <div class="card gradient-bloody">
-              <div class="card-body p-4">
-                <div class="media">
-                <div class="media-body text-left">
-                  <h4 class="text-white">{{ money_format_india($expenses->sum('amount')) }}</h4>
-                  <span class="text-white">Total Expenses</span>
+                <div class="card-body p-4">
+                    <div class="media">
+                        <div class="media-body text-left">
+                            <h4 class="text-white">{{ money_format_india($expenses->sum('amount')) }}</h4>
+                            <span class="text-white">Total Expenses</span>
+                        </div>
+                        <div class="align-self-center w-icon"><i class="icon-wallet text-white"></i></div>
+                    </div>
                 </div>
-                <div class="align-self-center w-icon"><i class="icon-wallet text-white"></i></div>
-              </div>
-              </div>
             </div>
-          </div>
+        </div>
 
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-body">
+                    <div class="d-flex justify-content-between mb-3">
+                        <div>
+                            <input type="date" class="form-control" wire:model="expense_date">
+                        </div>
+                        <button type="button" class="btn btn-outline-primary waves-effect waves-light" data-toggle="modal" data-target="#modal"><i class="fa fa-plus mr-1"></i> Create </button>
+                    </div>
                     <div class="table-responsive">
                         <table class="table">
                             <thead class="thead-primary shadow-primary">
@@ -52,7 +53,12 @@
                                     </td>
                                     <td>
                                         @foreach ($expenses->where('category_id', $expense_category->id) as $expense)
-                                        <span class="badge badge-danger m-1" title="{{ $expense->title ?? 'N/A' }}"> {{ $expense->amount }}</span> 
+                                        <span class="badge badge-danger m-1" title="{{ $expense->title ?? 'N/A' }}">
+                                            {{ $expense->amount }} TK
+                                            <a href="javascript:void(0)" wire:click="select_for_delete({{ $expense->id }})" data-toggle="modal" data-target="#delete_modal" title="Delete" class="badge badge-success ml-1">
+                                                <i class="fa fa-trash"></i>
+                                            </a>
+                                        </span>
                                         @endforeach
                                     </td>
                                 </tr>
@@ -87,11 +93,11 @@
                                         {{ $expense_category->name }}
                                     </td>
                                     <td>
-                                        <input type="text" class="form-control @error("expense_fields.$expense_category->id.title") bg-danger @enderror" placeholder="Title" wire:model="expense_fields.{{ $expense_category->id }}.title">
+                                        <input type="text" class="form-control @error(" expense_fields.$expense_category->id.title") bg-danger @enderror" placeholder="Title" wire:model="expense_fields.{{ $expense_category->id }}.title">
                                     </td>
                                     <td>
                                         <div class="input-group mb-3">
-                                            <input type="number" class="form-control @error("expense_fields.$expense_category->id.amount") bg-danger @enderror" placeholder="Amount" wire:model="expense_fields.{{ $expense_category->id }}.amount">
+                                            <input type="number" class="form-control @error(" expense_fields.$expense_category->id.amount") bg-danger @enderror" placeholder="Amount" wire:model="expense_fields.{{ $expense_category->id }}.amount">
                                             <div class="input-group-append">
                                                 <button class="btn btn-outline-primary" type="button" wire:click="add_expense({{ $expense_category->id }})">+</button>
                                             </div>
@@ -104,6 +110,18 @@
                     </div>
                 </div>
 
+            </div>
+        </div>
+    </div>
+    <!-- Delete Modal -->
+    <div wire:ignore.self class="modal fade" id="delete_modal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-body text-center">
+                    <p>Are you sure want to delete?</p>
+                    <button type="button" class="btn btn-secondary close-btn" data-dismiss="modal">Close</button>
+                    <button type="button" wire:click.prevent="delete()" class="btn btn-danger close-modal" data-dismiss="modal">Yes, Delete</button>
+                </div>
             </div>
         </div>
     </div>
