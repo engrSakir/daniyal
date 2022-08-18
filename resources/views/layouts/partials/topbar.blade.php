06e54d22ff031bf2a43c2f3d1c4c338a0f6de4d7
unknown <sakir.py>
2022-08-18 08:54:57 +0600

    rm image

--- resources/views/layouts/partials/topbar.blade.php
+++ resources/views/layouts/partials/topbar.blade.php
@@ -13,13 +13,13 @@
                     
                     <li class="nav-item">
                         <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" data-toggle="dropdown" href="#">
-                            <span class="user-profile"><img src="{{ asset('assets/images/avatars/avatar-17.png') }}" class="img-circle" alt="user avatar"></span>
+                            <span class="user-profile"><img src="{{ asset('assets/images/user.png') }}" class="img-circle" alt="user avatar"></span>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-right animated fadeIn">
                             <li class="dropdown-item user-details">
                                 <a href="{{ route('manager.profile') }}">
                                     <div class="media">
-                                        <div class="avatar"><img class="align-self-start mr-3" src="{{ asset('assets/images/avatars/avatar-17.png') }}" alt="user avatar"></div>
+                                        <div class="avatar"><img class="align-self-start mr-3" src="{{ asset('assets/images/user.png') }}" alt="user avatar"></div>
                                         <div class="media-body">
                                             <h6 class="mt-2 user-title">{{ auth()->user()->name }}</h6>
                                             <p class="user-subtitle">{{ auth()->user()->phone }}</p>
